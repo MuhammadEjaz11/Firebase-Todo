c10798583c3bf0c5a2163a6931ddd3fc18b8d77d
Muhammad Ejaz <mejaz4349@gmail.com>
2022-07-30 03:38:58 +0500

    complete UI

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
+
+
+
+
 //Add Task Function
 const addtask = document.getElementById('addtask');
 addtask.addEventListener('click', addtaskfn);
-function addtaskfn(){
+function addtaskfn() {
     let tasklist = document.createElement('div');
     var taskinput = document.getElementById('taskinputhtml');
-    tasklist.setAttribute("class","tasklist" );
+    tasklist.setAttribute("class", "tasklist");
     var parent = document.getElementById('parenttask');
-    if(taskinput.value == ""){
+    if (taskinput.value == "") {
         alert("Please Add Task")
     }
-    else{
+    else {
         tasklist.innerHTML = `
-        <p>${taskinput.value}</p> <button onclick="completeFn(this)"  class="btnn">Mark as Complete</button>`;
+        <p>${taskinput.value}</p> <div class="cbtn">
+        <button onclick="completeFn(this)" class="btnn">Mark as Completed</button>
+        <button  onclick="cleartasks(this)" class="btnn" id="clearbtn">Clear All Task</button>
+      </div>`;
         parent.appendChild(tasklist);
-        
-        
+
+
         taskinput.value = "";
         console.log(taskinput)
 
     }
 }
-//Complete Tas Function
+//Complete Task Function
 
-function completeFn(e){
-e.innerHTML = "Completed"
+function completeFn(e) {
+    e.innerHTML = "Completed"
 }
+//cleartask button
+
+function cleartasks(e) {
+    e.parentElement.parentElement.remove();
+}
+// //clear Task Function
+// var clearbtn = document.getElementById('clearbtn').addEventListener("click", clearAllTask);
+// function clearAllTask(){
+//     var parent = document.getElementById('parenttask');
+//     // parent.tasklist.remove();
+//     console.log(parent.tasklist)
+// }
+
+
+
